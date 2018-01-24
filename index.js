c9efdf837c4b0e7c98fa662f2af425924db7173d
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-01-23 17:28:27 -0800

    add new route to fetch business info

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+const url = require('url')
 const express = require('express')
 const app = express()
 
-app.get('/', async (req, res) => {
+app.get('/business-info', async (req, res) => {
 	try {
-		const url = require('url')
+		const requestPromise = require('request-promise-native')
+		const cnpjToSearch = url.parse(req.url, true).query.cnpj
+		const result = await requestPromise(`https://zirocnpj.now.sh?cnpj=${cnpjToSearch}`)
+		res.end(result)
+	} catch (error) {
+		console.log(error)
+		res.end(error)
+	}
+})
+
+app.get('/inscricao-estadual', async (req, res) => {
+	try {
 		const sefaz = require('./functions/sefaz')
 		const cnpjToSearch = url.parse(req.url, true).query.cnpj
 		const result = await sefaz(cnpjToSearch)
 		if (result === 'error')
 			res.end('Error executing scraper')
-		console.log(result)
 		res.end(result)
 	} catch (error) {
 		console.log(error)
